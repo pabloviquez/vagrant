d15dda2fae0bb4bd877d853380980f8138acafc0
Pablo Viquez <pablo.viquez@pviquez-10765.local>
2015-09-14 09:24:08 -0600

    Initiated work, setting scripts for provisioning a brand new VM

---
+++ apache/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# -- Apache
+# -----------------------------------------------------------------------------
+
+
+install_apache()
+{
+	echo "Adding Apache2 repository"
+	sudo apt-add-repository ppa:ptn107/apache
+	sudo apt-get update
+
+	echo "Installing Apache2"
+	apt-get -y -V install apache2
+
+	echo "Enabling Apache Extensions"
+	# apt-get install libapache2-mpm-itk apache2-mpm-itk
+
+	sudo a2enmod proxy
+	sudo a2enmod rewrite
+	sudo a2enmod headers
+	sudo service apache2 restart
+}
+
+CMDA=$(apache2 -v 2>&1)
+if [[ $CMDA != *"Apache server"* ]]
+then
+	install_apache
+fi
---
+++ setup.sh
@@ -0,0 +1,10 @@
+# -----------------------------------------------------------------------------
+# -- Ubuntu Install Script
+# -- 
+# -- @author Pablo Viquez
+# -----------------------------------------------------------------------------
+#!/bin/bash
+
+source base/install.sh
+source apache/install.sh
+source php/install.sh
