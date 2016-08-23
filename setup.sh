42a6f26332d7dd0eac12c1b1715bf0f9dc0c24b6
Pablo Viquez <pviquez@pabloviquez.com>
2016-08-22 18:32:38 -0600

    Added tag to clone

--- setup.sh
+++ setup.sh
@@ -28,7 +28,8 @@ fi
 
 echo "Cloning remote provisioning"
 rm -Rf ~/provision
-git clone https://github.com/pabloviquez/vagrant.git ~/provision
+# git clone https://github.com/pabloviquez/vagrant.git ~/provision
+git clone --branch 1.1.3 https://github.com/pabloviquez/vagrant.git
 
 if [ -d ~/provision ]; then
   echo "Setting up dependencies"
